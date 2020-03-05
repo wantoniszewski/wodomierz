49583ad1a047de41845ab54c8f71ec55dcbbd2e1
Mar cin <39875093+tweaknet@users.noreply.github.com>
2020-03-05 20:34:32 +0100

    calss1

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wodomierz
+{
+    class Class1
+    {
+        public void klasaKtoraMoznaUsunac()
+        {
+            Console.WriteLine("hej Marcin");
+        }
+    }
+}
