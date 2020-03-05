8d8dbe12e861e316cad672096806d59add1a8e79
Mar cin <39875093+tweaknet@users.noreply.github.com>
2020-03-05 21:28:35 +0100

    1

--- Class1.cs
+++ Class1.cs
@@ -9,7 +9,7 @@ namespace wodomierz
         public void klasaKtoraMoznaUsunac()
         {
             Console.WriteLine("hej Marcin");
-            global::System.Console.WriteLine("marcin");
+            global::System.Console.WriteLine("marcin_ateraz");
         }
     }
 }
